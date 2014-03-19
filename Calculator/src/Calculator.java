f309c199edccb2c9760da19cd528b8988518e4f4
Jonathan-Rico <jonyrico231@gmail.com>
2014-03-18 19:42:40 -0700

    Calculator for integers

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+public class Calculator {
+	public static void main(String [] args){
+	
+		System.out.printf("Hello, please enter two integer numbers below.\n");
+		Scanner input = new Scanner(System.in);
+		int x = input.nextInt();
+		int y = input.nextInt();
+	
+		System.out.printf("Please enter Add ,  Subtract , Multiply or Divide for the program to perform an arithmetic function with the numbers given\n");
+		String function = input.next();
+		
+		if (function.equals("Add")) {
+			int sum = x+y;
+			System.out.printf("The result is %d", sum);
+			}
+		else if (function.equals("Subtract")){
+			int subtract = x-y;
+			System.out.printf("The result is %d", subtract);
+			}
+		else if (function.equals("Multiply")){
+			int multiply = x*y;
+			System.out.printf("The result is %d", multiply);
+			}
+		else if (function.equals("Divide")){
+			int divide = x/y;
+			System.out.printf("The result is %d", divide);
+		}
+	}
+}
+
